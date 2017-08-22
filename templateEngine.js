62ff54ff6c064bf14cb7468e664e5f30f8b6e10c
Mikhail Cheberya <mikhail@idope.ru>
2017-08-22 23:18:10 +0300

    убрал экранирование где оно не обязательно

--- templateEngine.js
+++ templateEngine.js
@@ -2,22 +2,22 @@ function render( template, data = {} ) {
 	let output = template;
 
 	// подстановки
-	const subRegexp = /\{\{\s*([A-z_][A-z0-9_]+)\s*\}\}/g;
+	const subRegexp = /{{\s*([A-z_][A-z0-9_]+)\s*}}/g;
 	output = output.replace( subRegexp, (match, name) => data[ name ] || '' );
 
 	// комментарии
-	const comRegexp = /\{#(.*)#\}/g;
+	const comRegexp = /{#(.*)#}/g;
 	output = output.replace( comRegexp, (match, comment) => '' );
 
 	// блоки
-	const blockRegexp = /(\{%\s*[A-z_][A-z0-9_]+\s*%\}|\{%\s*\/\s*%\}|\{\{\s*\.\s*\}\})/g;
+	const blockRegexp = /({%\s*[A-z_][A-z0-9_]+\s*%}|{%\s*\/\s*%}|{{\s*\.\s*}})/g;
 	output.split( blockRegexp ).forEach( part => {
 		var match;
-		if (part.match(/\{\{\s*\.\s*\}\}/)) {
+		if (part.match(/{{\s*\.\s*}}/)) {
 			console.log( 'element' );
-		} else if (part.match(/\{%\s*\/\s*%\}/)) {
+		} else if (part.match(/{%\s*\/\s*%}/)) {
 			console.log( 'closing' );
-		} else if (match = part.match(/\{%\s*([A-z_][A-z0-9_]+)\s*%\}/)) {
+		} else if (match = part.match(/{%\s*([A-z_][A-z0-9_]+)\s*%}/)) {
 			console.log( 'open ' + match[1] );
 		} else {
 			console.log( part );
