bb08f3a6384e10b808a0f40e0d427538cdcf083e
Mikhail Cheberya <mikhail@idope.ru>
2017-08-22 23:15:31 +0300

    проход по блокам и определение кусков
    пока без обработки и вывода

--- templateEngine.js
+++ templateEngine.js
@@ -9,5 +9,20 @@ function render( template, data = {} ) {
 	const comRegexp = /\{#(.*)#\}/g;
 	output = output.replace( comRegexp, (match, comment) => '' );
 
+	// блоки
+	const blockRegexp = /(\{%\s*[A-z_][A-z0-9_]+\s*%\}|\{%\s*\/\s*%\}|\{\{\s*\.\s*\}\})/g;
+	output.split( blockRegexp ).forEach( part => {
+		var match;
+		if (part.match(/\{\{\s*\.\s*\}\}/)) {
+			console.log( 'element' );
+		} else if (part.match(/\{%\s*\/\s*%\}/)) {
+			console.log( 'closing' );
+		} else if (match = part.match(/\{%\s*([A-z_][A-z0-9_]+)\s*%\}/)) {
+			console.log( 'open ' + match[1] );
+		} else {
+			console.log( part );
+		}
+	});
+
 	return output;
 }
