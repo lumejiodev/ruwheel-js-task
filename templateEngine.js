4a71e0df75c1c3adb3c2f16cfff50073eb824ccf
Mikhail Cheberya <mikhail@idope.ru>
2017-08-22 05:52:34 +0300

    реализовал подстановки

--- templateEngine.js
+++ templateEngine.js
@@ -1,4 +1,11 @@
-function render( template, data ) {
+function render( template, data = {} ) {
 	let output = template;
+
+	// подстановки
+	const subRegexp = /\{\{\s*([A-z_][A-z0-9_]+)\s*\}\}/g;
+	output = output.replace( subRegexp, function( match, name ){
+		return data[ name ] || '';
+	});
+
 	return output;
 }
